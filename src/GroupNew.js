1cb08a4a762dac86eac2df11a1b87b266a7062cf
Kerem <tayhanek@gmail.com>
2022-04-12 17:26:39 +0300

    finito

--- src/GroupNew.js
+++ src/GroupNew.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
+import ToDoList from "./ToDoList";
+
 
 export default function GroupNew({ group }) {
 
   return (
     <>   
-    <h1>{group.label}</h1>
+    <h1>{group.label}{group.id}</h1>
     </>
   );
 
